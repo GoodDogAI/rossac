074bf9fa0e31ed3ac8b1ff74b86c600d97b0b2ec
Victor Milovanov <lostfreeman@gmail.com>
2020-08-30 17:04:35 -0700

    extract images and cmd_vels from rosbags

---
+++ read_imgs.py
@@ -0,0 +1,47 @@
+import rosbag
+
+import json
+import argparse
+import png
+import os.path
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument("bags", nargs='+')
+args = arg_parser.parse_args()
+
+# class Vec3:
+#     x = float
+#     y = float
+#     z = float
+
+# class CmdVel:
+#     angular = Vec3
+#     linear = Vec3
+for bag_file in args.bags:
+    bag = rosbag.Bag(bag_file, 'r')
+    seen = set()
+
+    cmd_file = open(bag_file + ".cmd_vels", 'w')
+
+    for topic, msg, ts in bag.read_messages(['/camera/color/image_raw', '/cmd_vel']):
+        #if topic not in seen:
+            full_ts = ts.nsecs + ts.secs * 1000000000
+            print("{} @ {}".format(topic, full_ts))
+            if topic == '/camera/color/image_raw':
+                print("{}x{} {}; row bytes: {}".format(msg.width, msg.height, msg.encoding, msg.step))
+                print(type(msg.data))
+                img_name = 'imgs/{}.png'.format(full_ts)
+                if not os.path.isfile(img_name):
+                    img = []
+                    for i in range(0, len(msg.data), msg.step):
+                        img.append(msg.data[i:i+msg.step])
+                    png.from_array(img, mode='RGB').save(img_name)
+            else:
+                print(dir(msg))
+                cmd_file.write('{{ "ts": "{}", "linear": [{},{},{}], "angular": [{},{},{}] }}\n'
+                                .format(full_ts, msg.linear.x, msg.linear.y, msg.linear.z,
+                                                msg.angular.x, msg.angular.y, msg.angular.z))
+            print()
+            seen.add(topic)
+
+    cmd_file.close()
