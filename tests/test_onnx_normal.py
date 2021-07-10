9967db205efdd579b47e6134594df5d110ef77e9
Jake Poznanski <jake@pozotron.com>
2021-07-10 16:51:41 -0700

    Just adding some unit tests on the custom ONNXNormal distribution

--- tests/test_onnx_normal.py
+++ tests/test_onnx_normal.py
@@ -5,16 +5,29 @@ import numpy as np
 from torch.distributions import Normal
 from onnx_normal_dist import ONNXNormal
 
+from hypothesis import given, strategies as st
+
 
 class TestONNXNormalDistribution(unittest.TestCase):
-    def test_sampling(self):
-        orig = Normal(loc=torch.Tensor([1.0]), scale=torch.Tensor([2.0]))
-        onnx = ONNXNormal(loc=torch.Tensor([1.0]), scale=torch.Tensor([2.0]))
+    @given(st.floats(min_value=-1e9, max_value=1e9), st.floats(min_value=0, max_value=5))
+    def test_sampling(self, mean, stddev):
+        orig = Normal(loc=torch.Tensor([mean]), scale=torch.Tensor([stddev]))
+        onnx = ONNXNormal(loc=torch.Tensor([mean]), scale=torch.Tensor([stddev]))
 
-        orig_sample = orig.sample(sample_shape=(100_000,))
-        onnx_sample = onnx.sample(sample_shape=(100_000,))
+        orig_sample = orig.rsample(sample_shape=(100_000,))
+        onnx_sample = onnx.rsample(sample_shape=(100_000,))
 
-        np.testing.assert_almost_equal(np.mean(orig_sample.numpy()), 1.0, decimal=2)
-        np.testing.assert_almost_equal(np.mean(onnx_sample.numpy()), 1.0, decimal=2)
+        self.assertLessEqual(abs(np.mean(orig_sample.numpy()) - mean), abs(mean) * 0.1 + 0.1)
+        self.assertLessEqual(abs(np.mean(onnx_sample.numpy()) - mean), abs(mean) * 0.1 + 0.1)
+
+        self.assertLessEqual(abs(np.std(orig_sample.numpy()) - stddev), abs(mean) * 0.1 + 0.1)
+        self.assertLessEqual(abs(np.std(onnx_sample.numpy()) - stddev), abs(mean) * 0.1 + 0.1)
+
+    @given(st.floats(), st.floats(), st.floats())
+    def test_logprob(self, mean, stddev, value):
+        orig = Normal(loc=torch.Tensor([mean]), scale=torch.Tensor([stddev]))
+        onnx = ONNXNormal(loc=torch.Tensor([mean]), scale=torch.Tensor([stddev]))
+
+        np.testing.assert_almost_equal(orig.log_prob(value), onnx.log_prob(value))
 
 
