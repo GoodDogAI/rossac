d7d8b36f716e3d36a18f30590e71056edda815d6
Jake Poznanski <jake@pozotron.com>
2021-08-20 13:28:15 -0700

    Fixing tests to align with latest bot code

--- tests/test_bag_brain_io.py
+++ tests/test_bag_brain_io.py
@@ -6,7 +6,7 @@ import onnxruntime as rt
 
 class TestBagBrainIO(unittest.TestCase):
     def test_lstm_sample(self):
-        bag = rosbag.Bag("/home/jake/bagfiles/test_bags/record-brain-test-lstm-samp0.0_2021-08-20-10-08-08_0.bag", "r")
+        bag = rosbag.Bag("/home/jake/bagfiles/test_bags/record-brain-test-lstm-samp0.0_2021-08-20-13-13-06_0.bag", "r")
         onnx = rt.InferenceSession("/home/jake/bagfiles/test_bags/test-lstm.onnx")
         history_size = 8
 
@@ -20,25 +20,20 @@ class TestBagBrainIO(unittest.TestCase):
             outputs.append(np.array(msg.data))
 
         print("Loaded")
-
-        # Check that the first N brain inputs grow in size
-        self.assertTrue(all([len(x) == (i + 1) * len(inputs[0]) for i, x in enumerate(inputs[:history_size])]))
-        self.assertTrue(all([len(x) == history_size * len(inputs[0]) for x in inputs[history_size:]]))
+        print([len(x) for x in inputs])
 
         # Check all outputs the same size
         self.assertTrue(all(len(x) == len(outputs[0]) for x in outputs))
 
         # Check that inputs match outputs through
         for inp, out in zip(inputs, outputs):
-            inp = np.pad(inp, (0, len(inputs[-1]) - len(inp)))
-            inp = np.reshape(inp, (1, len(inp) // len(inputs[0]), len(inputs[0])))
+            inp = np.reshape(inp, (1, history_size, len(inp) // history_size))
             inp = inp.astype(np.float32)
 
             pred = onnx.run(["actions"], {
                 "yolo_intermediate": inp
             })
-            pred = pred[0] # Get the actual actions out
-
+            pred = pred[0][0] # First index to get the actions array as output, second to get first batch only
 
             print(np.linalg.norm(out - pred))
-            #np.testing.assert_almost_equal(out, pred)
+            np.testing.assert_almost_equal(out, pred, decimal=5)
