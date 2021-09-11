0474100e49051472bda8f7fa75c1c588fde1614b
Jake Poznanski <jake@pozotron.com>
2021-09-11 13:52:07 -0700

    Adding a script which makes it easy to add new intermediate outputs to an ONNX graph, with shape inference.

---
+++ yolo_onnx.py
@@ -0,0 +1,25 @@
+import numpy as np
+import onnx
+import onnx_graphsurgeon as gs
+from onnx import shape_inference, helper, GraphProto
+
+
+# This script will take a Yolov5s onnx export and adjust it to allow exporting an intermediate layer
+existing_onnx_path = "yolov5s.onnx"
+new_onnx_path = "yolov5s_trt.onnx"
+
+
+def add_intermediate_output(model: GraphProto, output_name:str) -> GraphProto:
+    intermediate_layer_value_info = helper.ValueInfoProto()
+    intermediate_layer_value_info.name = output_name
+    model.graph.output.append(intermediate_layer_value_info)
+
+    return onnx.shape_inference.infer_shapes(model)
+
+
+model = onnx.load_model(existing_onnx_path)
+model = add_intermediate_output(model, "361")
+onnx.save_model(model, new_onnx_path)
+
+
+onnx.checker.check_model(onnx.load(new_onnx_path), full_check=True)
