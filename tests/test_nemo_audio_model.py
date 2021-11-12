fb5af67855a098fc15aba661f2876c90e1d0f2f3
Jake Poznanski <jake@pozotron.com>
2021-11-11 18:26:36 -0800

    Got to the point where ONNX exports, but stft operations for the mel spectrogram are still not supported. So i'll be going back.

--- tests/test_nemo_audio_model.py
+++ tests/test_nemo_audio_model.py
@@ -8,17 +8,39 @@ from nemo.collections.asr.models import EncDecClassificationModel
 from torchaudio import transforms
 
 
+class FullyExportableEncDecClassificationModel(EncDecClassificationModel):
+    def _get_input_example(self):
+        return tuple([torch.rand(1, 48000 * 4).to(next(self.parameters()).device),
+                      torch.tensor([48000 * 4])])
+
+    def forward_for_export(self, input, length=None):
+        processed_signal, processed_signal_len = self.preprocessor(
+            input_signal=input, length=length,
+        )
+        # Crop or pad is always applied
+        if self.crop_or_pad is not None:
+            processed_signal, processed_signal_len = self.crop_or_pad(
+                input_signal=processed_signal, length=processed_signal_len
+            )
+        encoder_output = self.input_module(processed_signal, processed_signal_len)
+        if isinstance(encoder_output, tuple):
+            return self.output_module(encoder_output[0])
+        else:
+            return self.output_module(encoder_output)
+
+
 class TestNemoAudioModel(unittest.TestCase):
     sample_rate = 16_000
 
     def test_nemo_export(self):
-        asr_model = EncDecClassificationModel.from_pretrained("commandrecognition_en_matchboxnet3x2x64_v2")
+        asr_model = FullyExportableEncDecClassificationModel.from_pretrained("commandrecognition_en_matchboxnet3x2x64_v2")
 
         # Export the model
-        asr_model.export("asr_command_recognition.onnx", onnx_opset_version=12)
+        asr_model.export("asr_command_recognition.onnx",
+                         use_dynamic_axes=False)
 
     def test_nemo_sample_data(self):
-        wav, sr = soundfile.read(os.path.join(os.path.dirname(__file__), "test_data", "jake_clean_forward.wav"))
+        wav, sr = soundfile.read(os.path.join(os.path.dirname(__file__), "test_data", "victor_forward_48k.wav"))
         asr_model = EncDecClassificationModel.from_pretrained("commandrecognition_en_matchboxnet3x2x64_v2")
 
         wav = torch.from_numpy(wav).float()
