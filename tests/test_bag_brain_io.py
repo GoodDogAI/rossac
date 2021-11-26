546e82e552105cc4ab9a36cded32c767d425d572
Jake Poznanski <jake@pozotron.com>
2021-11-26 12:39:03 -0800

    Adding test to confirm that the yolo intermediate outputs are being computed correctly.

--- tests/test_bag_brain_io.py
+++ tests/test_bag_brain_io.py
@@ -4,6 +4,43 @@ import unittest
 import numpy as np
 import onnxruntime as rt
 
+from yolo_reward import get_onnx_prediction, non_max_supression, detect_yolo_bboxes
+
+
+class TestBagYoloIntermediate(unittest.TestCase):
+    test_bag = "/home/jake/bagfiles/test_bags/yolov5s_intermediate_color_test.bag"
+    onnx_path = os.path.join(os.path.dirname(__file__), "..", "yolov5s_trt.onnx")
+
+    def test_yolo_color(self):
+        bag = rosbag.Bag(self.test_bag, "r")
+        onnx_sess = rt.InferenceSession(self.onnx_path)
+
+        inputs = []
+        outputs = []
+
+        for topic, msg, ts in bag.read_messages(["/processed_img"]):
+            img = []
+            for i in range(0, len(msg.data), msg.step):
+                img.append(np.frombuffer(msg.data[i:i + msg.step], dtype=np.uint8))
+            image_np = np.array(img)
+            image_np = image_np.reshape((480, 640, 3))
+            inputs.append(image_np)
+
+        for topic, msg, ts in bag.read_messages(["/yolo_intermediate"]):
+            outputs.append(np.array(msg.data))
+
+        print(f"Loaded {len(inputs)} inputs, {len(outputs)} outputs")
+
+        # Check that inputs match outputs through
+        for inp, out in zip(inputs, outputs):
+            bboxes, intermediate = get_onnx_prediction(onnx_sess, inp)
+            intermediate = intermediate.reshape(-1)
+            nms_boxes = non_max_supression(bboxes)
+            detections = detect_yolo_bboxes(nms_boxes, threshold=0.25)
+
+            print("Diff: ", np.linalg.norm(out - intermediate))
+            np.testing.assert_almost_equal(out, intermediate, decimal=4)
+
 
 
 class TestBagBrainIO(unittest.TestCase):
